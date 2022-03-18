237f8115dee791d39c7f912c49823df9ab84034d
poeta_io <ilya.10189@gmail.com>
2022-03-18 10:56:34 +0200

    Change curfew hours

--- src/main/java/bots/utils/Constants.java
+++ src/main/java/bots/utils/Constants.java
@@ -4,8 +4,8 @@ public interface Constants {
     String BOT_USERNAME = "PidvezyBot";
     int CREATOR_ID = 278171783;
     int DRIVER_UPDATE_INTERVAL = 20;
-    int CURFEW_START_HOUR = 18;
-    int CURFEW_END_HOUR = 5;
+    int CURFEW_START_HOUR = 22;
+    int CURFEW_END_HOUR = 14;
 
     String DRIVER_STATES = "DRIVER_STATES";
     String PASSENGER_STATES = "PASSENGER_STATES";
